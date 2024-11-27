80cc88648a63cff9db0dd674420661d7cb7e61ba
Kosy <kosysteve03@gmail.com>
2024-11-27 00:20:20 +0000

    Populating model classes

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -2,5 +2,23 @@
 {
     public class Customer
     {
+        public int CustomerId { get; set; }  // Primary Key
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }  // Unique field for login
+        public string PhoneNumber { get; set; }
+        public string PasswordHash { get; set; }  // For secure authentication
+        public string Salt { get; set; }
+        public string Role { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime CreatedAt { get; set; }  // Timestamp
+        public DateTime UpdatedAt { get; set; }  // Timestamp
+
+        // Navigation Property to Orders
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
     }
 }
---
+++ Models/Image.cs
@@ -0,0 +1,12 @@
+﻿namespace SOA_CA2_E_Commerce.Models
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public int ProductId { get; set; }
+        public string fileName { get; set; }
+        public string Link { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
