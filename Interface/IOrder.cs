ba281d8bfeab86ec65c78535be2da716f9dfde32
Raveena Rathnam <118574384+RaveenaRathnam@users.noreply.github.com>
2024-11-29 23:29:19 +0000

    Update IOrder.cs
    Defined the Operations that have to be implemented in the Order Service.

--- Interface/IOrder.cs
+++ Interface/IOrder.cs
@@ -1,6 +1,19 @@
-﻿namespace SOA_CA2_E_Commerce.Interface
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SOA_CA2_E_Commerce.DTO;
+
+namespace SOA_CA2_E_Commerce.Interface
 {
     public interface IOrder
     {
+        // CRUD Operations
+        Task<IEnumerable<OrdersDTO>> GetAllOrdersAsync();
+        Task<OrdersDTO> GetOrderByIdAsync(int id);
+        Task CreateOrderAsync(OrdersDTO orderDTO);
+        Task UpdateOrderAsync(int id, OrdersDTO orderDTO);
+        Task DeleteOrderAsync(int id);
+
+        // Custom Operation
+        Task<IEnumerable<OrdersDTO>> GetOrdersByStatusAsync(string status);
     }
 }
