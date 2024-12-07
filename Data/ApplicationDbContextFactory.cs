93021b3d1fee58e018c7d4b49f8bfd2b53d51708
Kosy <kosysteve03@gmail.com>
2024-12-07 16:08:44 +0000

    Taking off test file to alllow hosting

---
+++ Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace SOA_CA2_E_Commerce.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            // Load configuration from appsettings.json
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Get the connection string
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            // Configure EF Core to use SQL Server
+            optionsBuilder.UseSqlServer(connectionString,options =>
+              options.EnableRetryOnFailure(
+        maxRetryCount: 5,
+        maxRetryDelay: TimeSpan.FromSeconds(30),
+        errorNumbersToAdd: null));
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
